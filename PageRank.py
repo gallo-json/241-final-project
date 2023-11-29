eaebe9854a098942ed1560128f2bba3a0e9e4cf8
JDMADESP <152448238+JDMADESP@users.noreply.github.com>
2023-11-29 17:33:12 -0500

    HOLA

---
+++ PageRank.py
@@ -0,0 +1,40 @@
+markovMatrix = [[0.0, 0.5, 0.333333],
+                [0.0, 0.0, 0.333333],
+                [1.0, 0.5, 0.333333]]
+
+vector = [1, 4, 7, 5, 2, 9, 11]
+newVector = sorted(vector)
+print(newVector)
+
+
+# dampeningFactor = 0.85
+# randomPageProb = 1 - dampeningFactor
+# numberStates = len(markovMatrix)
+# markovMatrixPrime = []
+# for row in markovMatrix:
+#     newRow = []
+#     for val in row:
+#         firstHalf = randomPageProb * val
+#         secondHalf = (dampeningFactor / numberStates)
+#         newRow.append(firstHalf + secondHalf)
+#     markovMatrixPrime.append(newRow)
+# markovMatrix = markovMatrixPrime
+# # print(markovMatrix)
+
+# surfDistribution = []
+# lengthMarkov = len(markovMatrix[0])
+# startingProbs = 1 / lengthMarkov
+# for val in range(lengthMarkov):
+#     surfDistribution.append(startingProbs)
+# newSurfers = []
+# while surfDistribution != newSurfers:
+#     newSurfers = []
+#     for row in range(len(markovMatrix)):
+#         sum = 0
+#         for col in range(len(markovMatrix[0])):
+#             newVal = markovMatrix[row][col] * surfDistribution[col]
+#             sum += newVal
+#         newSurfers.append(sum)
+#     surfDistribution = newSurfers
+# print(surfDistribution)
+
